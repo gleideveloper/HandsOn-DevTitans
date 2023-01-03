f768bc215ce4a99eba653caede03633f13178349
Kellem <ksales89@gmail.com>
2023-01-02 20:12:36 -0400

    Base KotlinRecyclerView

---
+++ batscan/BatscanApp/app/src/main/java/com/example/batscanapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.batscanapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var liveAdapter: LiveAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initRecyclerView()
+        addDataSource()
+
+    }
+
+    private fun initRecyclerView() {
+
+        liveAdapter = LiveAdapter { live ->
+            openLink(live.link)
+        }
+
+        recyclerview.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = liveAdapter
+        }
+
+    }
+
+    private fun openLink(link: String) {
+
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+        startActivity(browserIntent)
+
+    }
+
+    private fun addDataSource() {
+
+        val dataSource = DataSource.createDataSet()
+        liveAdapter.setList(dataSource)
+
+    }
+
+}
---
+++ batscan/BatscanApp/app/src/main/java/com/example/batscanapp/models/Live.kt
@@ -0,0 +1,11 @@
+package com.example.batscanapp.models
+
+data class Live (
+
+    var title: String,
+    var author: String,
+    var thumbnailUrl: String,
+    var link: String
+
+        ){
+}

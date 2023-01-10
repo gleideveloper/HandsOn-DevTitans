f667ac5e931e1a04830255587a7c22366470000a
Kellem <ksales89@gmail.com>
2023-01-10 18:07:35 -0400

    limpando e app funcionando dados locais

---
+++ batscan/BatScanAppFinal/app/src/main/java/devtitans/batscanapp/EquipamentoDAO.java
@@ -0,0 +1,130 @@
+package devtitans.batscanapp;
+
+import static android.content.ContentValues.TAG;
+
+import static devtitans.batscanapp.Conexao.getDadosVendor;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import kotlinx.coroutines.internal.SystemPropsKt;
+
+//public class EquipamentoDAO {
+//    private Context context;
+//    private IBinder binder;
+//    private IBatscan service;
+//    private String textStatus, textRssiMac;
+//
+//
+//    private static ArrayList<Equipamento> equipamentoList = new ArrayList<>();
+//
+//    public EquipamentoDAO(Context context) {
+//        this.context = context;
+//    }
+//
+//
+//    public ArrayList<Equipamento> getEquipamentoList() {
+//        if (equipamentoList.size() == 0) {
+//            for (int i = 0; equipamentoList.size() < 5; i++) {
+//                textRssiMac = findEquipamento();
+//                equipamentoList.add(new Equipamento(i, textRssiMac, textRssiMac, "vendor"));
+//            }
+//
+//        }
+//        return equipamentoList;
+//    }
+//
+//    public boolean add(Equipamento equipamento) {
+//        equipamento.setId(equipamentoList.size());
+//        equipamentoList.add(equipamento);
+//        Toast.makeText(context, "Equipamento adicionado!", Toast.LENGTH_SHORT).show();
+//        return true;
+//    }
+//
+//    public Equipamento get(int id) {
+//        return equipamentoList.get(id);
+//    }
+//
+//    public String findEquipamento() {
+//        binder = ServiceManager.getService("devtitans.batscan.IBatscan/default"); // Acessa e consulta o binder
+//        if (binder != null) {
+//            service = IBatscan.Stub.asInterface(binder);                   // Acessa o serviço batscan
+////            if (service != null)
+////                Toast.makeText(this, "Serviço batscan acessado com sucesso.", Toast.LENGTH_LONG).show();
+////            else
+////                Toast.makeText(this, "Erro ao acessar o serviço batscan!", Toast.LENGTH_LONG).show();
+////        }
+////        else
+////            Toast.makeText(this, "Erro ao acessar o Binder!", Toast.LENGTH_LONG).show();
+//
+//            try {
+//                textRssiMac.service.getScan();                    // Executa o método getScan via IPC
+//
+//                int status = service.connect();                              // Executa o método connect via IPC
+//                if (status == 0) {
+//                    textStatus = "Desconectado";
+//                    //textStatus.setTextColor(Color.parseColor("#73312f"));
+//                } else if (status == 1) {
+//                    textStatus = "Conectado";
+//                    //textStatus.setTextColor(Color.parseColor("#6d790c"));
+//                } else {
+//                    textStatus = "Simulado";
+//                    textRssiMac = "00:11:11:|-86";
+//                    //textStatus.setTextColor(Color.parseColor("#207fb5"));
+//                }
+//
+//            } catch (android.os.RemoteException e) {
+//                //Toast.makeText(this, "Erro ao acessar o Binder!", LENGTH_LONG).show();
+//                Log.e(TAG, "Erro atualizando dados:", e);
+//            }
+//
+//            return textRssiMac;
+//        }
+//        return textRssiMac;
+//    }
+//}
+public class EquipamentoDAO {
+    private Context context;
+    private IBinder binder;
+    private String textStatus, textRssiMac, textVendor, textRssi, textMac;
+
+
+    private static ArrayList<Equipamento> equipamentoList = new ArrayList<>();
+
+    public EquipamentoDAO(Context context) {
+        this.context = context;
+    }
+
+
+    public ArrayList<Equipamento> getEquipamentoList() {
+        if (equipamentoList.size() == 0) {
+            for (int i = 0; equipamentoList.size() < 2; i++) {
+                textRssiMac = findEquipamento();
+                textMac = textRssiMac.substring(4,21);
+                textRssi = textRssiMac.substring(0,3);
+                textVendor = Conexao.getDadosVendor("https://api.macvendors.com/"+textRssiMac.substring(4,20));
+                equipamentoList.add(new Equipamento(i, textMac, textRssi, textVendor, false));
+                Log.i("app ","rssi = "+textRssi);
+                Log.i("app ","mac = "+textMac);
+                Log.i("app ","vendor = "+textVendor);
+
+            }
+
+        }
+        return equipamentoList;
+    }
+
+    public String findEquipamento() {
+        textStatus = "Simulado";
+        textRssiMac = "-86|00:1B:44:11:3A:B7";
+        return textRssiMac;
+    }
+
+
+}
--- batscan/BatscanTestAppRecycler/app/src/main/java/com/example/batscanapp/models/Equipment.kt
+++
@@ -1,9 +0,0 @@
-package com.example.batscanapp.models
-
-data class Equipment (
-
-    var vendor: String,
-    var rssi_mac: String
-
-        ){
-}

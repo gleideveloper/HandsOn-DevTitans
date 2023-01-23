774f3d4fa60bbd3e41e49d44866ca27f05fd63cb
Gleides Vinente <gleidevelop@gmail.com>
2023-01-23 01:27:26 -0400

    Add gitignore

--- batscan/ProjetoFinalBatscan/MyClientAppBatscan/app/src/main/java/devtitans/batscanapp/MacVendorListActivity.java
+++ batscan/ProjetoFinalBatscan/MyClientAppBatscan/app/src/main/java/devtitans/batscanapp/MacVendorListActivity.java
@@ -1,24 +1,17 @@
 package devtitans.batscanapp;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
-import android.os.Bundle;
-
-import devtitans.batscanapp.adapter.MacVendorAdapter;
-import devtitans.batscanapp.service.batscan.ServiceBatscan;
-import devtitans.batscanapp.service.batscan.response.MacRssiBatscan;
-import devtitans.batscanapp.service.network.response.MacVendorApiService;
-import devtitans.batscanapp.service.network.ApiServiceRetrofit;
-import devtitans.batscanapp.service.network.MacVendorApi;
-import devtitans.batscanapp.service.network.ServiceRetrofit;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import devtitans.batscanmanager.BatscanManager;
+import devtitans.batscanapp.adapter.MacVendorAdapter;
+import devtitans.batscanapp.service.network.ApiServiceRetrofit;
+import devtitans.batscanapp.service.network.response.MacVendorApiService;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -36,8 +29,6 @@ public class MacVendorListActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.mac_vendor_recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         getMacVendorApiServiceList();
-//        macVendorAdapter = new MacVendorAdapter(macVendorApiServiceList);
-//        recyclerView.setAdapter(macVendorAdapter);
     }
 
     private void getMacVendorApiServiceList() {
@@ -48,7 +39,7 @@ public class MacVendorListActivity extends AppCompatActivity {
                     public void onResponse(Call<List<MacVendorApiService>> call, Response<List<MacVendorApiService>> response) {
                         if(response.isSuccessful()) {
                             assert response.body() != null;
-                            macVendorApiServiceList = new ArrayList<>(response.body());
+                            populateListView(response.body());
                         }
                     }
 
@@ -59,8 +50,8 @@ public class MacVendorListActivity extends AppCompatActivity {
                 });
     }
     private void populateListView(List<MacVendorApiService> body) {
-
-//        macVendorAdapter = new MacVendorAdapter(body);
-//        recyclerView.setAdapter(macVendorAdapter);
+        macVendorApiServiceList = body;
+        macVendorAdapter = new MacVendorAdapter(macVendorApiServiceList);
+        recyclerView.setAdapter(macVendorAdapter);
     }
 }

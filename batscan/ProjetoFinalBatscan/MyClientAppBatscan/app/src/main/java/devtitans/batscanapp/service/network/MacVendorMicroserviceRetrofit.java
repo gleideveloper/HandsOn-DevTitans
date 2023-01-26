ca2172661708a078a3071214f40733cd227fe61f
Gleides Vinente <gleidevelop@gmail.com>
2023-01-26 18:34:46 -0400

    Implementando microserviço na nuvem com Heroku

--- batscan/ProjetoFinalBatscan/MyClientAppBatscan/app/src/main/java/devtitans/batscanapp/service/network/MacVendorMicroserviceRetrofit.java
+++ batscan/ProjetoFinalBatscan/MyClientAppBatscan/app/src/main/java/devtitans/batscanapp/service/network/MacVendorMicroserviceRetrofit.java
@@ -8,7 +8,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MacVendorMicroserviceRetrofit {
     private static MacVendorApi INSTANCE;
     private static String BASE_URL_DEVTITANS = "http://10.208.1.63:8080";
-    private static String BASE_URL = "http://192.168.15.57:8080";
+    private static String BASE_URL_LOCALHOST = "http://192.168.15.57:8080";
+    private static String BASE_URL = "https://api-rest-vendors.herokuapp.com";
 
     public static MacVendorApi getInstance() {
         if (INSTANCE == null) {

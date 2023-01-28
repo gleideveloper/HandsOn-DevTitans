156cf42079950dfe6d74c0ca25865049cdd316d0
Gleides Vinente <gleidevelop@gmail.com>
2023-01-28 02:24:32 -0400

    Implementação BroadcastReceiver Estado Esp32

--- batscan/ProjetoFinalBatscan/MyClientAppBatscan/app/src/main/java/devtitans/batscanapp/MainActivity.java
+++ batscan/ProjetoFinalBatscan/MyClientAppBatscan/app/src/main/java/devtitans/batscanapp/MainActivity.java
@@ -1,11 +1,16 @@
 package devtitans.batscanapp;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,29 +30,51 @@ public class MainActivity extends AppCompatActivity {
         statusBatscan = findViewById(R.id.txvStatusBatscan);
         btnBatScan = findViewById(R.id.btnScannear);
         serviceBatscan = new ServiceBatscan();
-    }
 
+        /*IntenFilter do estado da conexção UBS*/
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.hardware.usb.action.USB_STATE");
+        registerReceiver(receiver, filter);
+    }
     @Override
     protected void onResume() {
         super.onResume();
-        checkIsConnectedEsp32();
         homeSeachIcon.setOnClickListener(view -> checkIsConnectedEsp32());
+    }
+    public void deviceList(View view) {
+        Intent intent = new Intent(this, MacVendorListActivity.class);
+        startActivity(intent);
     }
 
     private void checkIsConnectedEsp32() {
         if (serviceBatscan.isConnected()) {
             homeSeachIcon.setImageResource(R.drawable.home_search_on);
             statusBatscan.setText("Conectado com Esp32!");
+            Toast.makeText(MainActivity.this, "ESP32 Connected!!!", Toast.LENGTH_SHORT).show();
+            statusBatscan.setTextColor(Color.BLUE);
             btnBatScan.setEnabled(true);
         } else {
             homeSeachIcon.setImageResource(R.drawable.home_search_off);
             statusBatscan.setText("Desconectado!");
+            statusBatscan.setTextColor(Color.RED);
+            Toast.makeText(MainActivity.this, "ESP32 disconnected!!!", Toast.LENGTH_SHORT).show();
             btnBatScan.setEnabled(false);
         }
     }
 
-    public void deviceList(View view) {
-        Intent intent = new Intent(this, MacVendorListActivity.class);
-        startActivity(intent);
-    }
+    /*BroadcastReceiver para ficar ouvindo a conexão USB*/
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getExtras().getBoolean("connected")) {
+                //start doing something for state - connected
+                checkIsConnectedEsp32();
+            } else {
+                //start doing something for state - disconnected
+                checkIsConnectedEsp32();
+            }
+        }
+    };
+
+
 }
